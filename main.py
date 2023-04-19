eac4bbb45ce6222b5a7eea047e7665ad7d12e2a5
Lucas Yudi N. Nishimoto <70718279+lucasnishimoto@users.noreply.github.com>
2023-04-19 11:47:55 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import mysql.connector
-import os
-import schedule
+import mysql.connector, os, schedule
 from icecream import ic
 from mysql.connector import Error
 from influxdb import InfluxDBClient
@@ -22,53 +20,48 @@ client = InfluxDBClient(host=variaveis['INFLUXDB_HOST'],
                         port=variaveis['INFLUXDB_PORT'], database=variaveis['INFLUXDB_DB'])
 
 
-def host():
+def container_fields():
     try:
-        consulta_sql = "SELECT state AS Status, json_image(data, '$.hostname') AS Hostname, json_image(data, '$.agentIpAddress') AS IP FROM host"
+        host_query = "SELECT state AS Status, json_image(data, '$.hostname') AS Hostname, json_image(data, '$.agentIpAddress') AS IP FROM host"
+        service_query = "SELECT service.name, account.name as environment, service.state as state, \
+      service.health_state, json_image(service.data, '$.imageUuid') as Image FROM service INNER JOIN \
+      environment ON service.environment_id = environment.ID INNER JOIN account ON environment.account_id = account.id GROUP BY state, environment;"
         cursor = con.cursor()
-        cursor.execute(consulta_sql)
-        linhas = cursor.fetchall()
+
+        cursor.execute(host_query)
+        host_rows = cursor.fetchall()
         print(f'Número total de registros retornados: , {cursor.rowcount}')
         print(f'\nMostrando os hosts cadastrados')
 
-        for linha in linhas:
+        for row in host_rows:
             data = [{
                 "measurement": "rancher_host",
                 "tags": {
                     "Ambiente": variaveis['ENVIRONMENT']
                 },
                 "fields": {
-                    'State': linha[0],
-                    'Hostname': linha[1],
-                    'IP': linha[2]
+                    'State': row[0],
+                    'Hostname': row[1],
+                    'IP': row[2]
                 }
             }]
             ic(data)
             client.write_points(data)
-    except Error as e:
-        print(f'Erro ao acessar tabela MySQL, {e}')
-######################################################################################################################
 
-
-def service():
-    try:
-        consulta_sql = "select service.name, account.name as environment, service.state as state, \
-      service.health_state, json_image(service.data, '$.imageUuid') as Image from service 	inner join \
-      environment on service.environment_id = environment.ID inner join account on environment.account_id = account.id group by state, environment;"
-        cursor = con.cursor()
-        cursor.execute(consulta_sql)
-        linhas = cursor.fetchall()
+        cursor.execute(service_query)
+        service_rows = cursor.fetchall()
         print(f'Número total de registros retornados: , {cursor.rowcount}')
         print(f'\nMostrando os serviços cadastrados')
-        for linha in linhas:
+
+        for row in service_rows:
             data = [{
                 "measurement": "rancher_service",
                 "fields": {
-                    'Name': linha[0],
-                    'Environment': linha[1],
-                    'State': linha[2],
-                    'Health_state': linha[3],
-                    'Image': linha[4],
+                    'Name': row[0],
+                    'Environment': row[1],
+                    'State': row[2],
+                    'Health_state': row[3],
+                    'Image': row[4],
                 },
                 "tags": {
                     "Ambiente": variaveis['ENVIRONMENT']
@@ -76,13 +69,13 @@ def service():
             }]
             ic(data)
             client.write_points(data)
+
     except Error as e:
         print(f'Erro ao acessar tabela MySQL, {e}')
 
 
 def main():
-    schedule.every(0.9).minutes.do(host)
-    schedule.every(0.9).minutes.do(service)
+    schedule.every(0.9).minutes.do(container_fields)
     while True:
         schedule.run_pending()
 
